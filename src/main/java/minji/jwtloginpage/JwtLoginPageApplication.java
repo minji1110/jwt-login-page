bbf88446a4e872ea60808c731697d713e1f63873
minji1110 <kalswl991110@gmail.com>
2021-01-20 16:30:25 +0900

    Initial commit

---
+++ src/main/java/minji/jwtloginpage/JwtLoginPageApplication.java
@@ -0,0 +1,13 @@
+package minji.jwtloginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLoginPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtLoginPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/minji/jwtloginpage/JwtLoginPageApplicationTests.java
@@ -0,0 +1,13 @@
+package minji.jwtloginpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtLoginPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
